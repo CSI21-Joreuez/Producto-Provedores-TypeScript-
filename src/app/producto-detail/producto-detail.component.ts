9b937b3d2a7658a8ce404da291bc77b55db2973d
Juan Carlos Orea Rodríguez <csi21-joreuez@colegioaltair.net>
2022-11-07 22:13:31 +0100

    Main

--- src/app/producto-detail/producto-detail.component.ts
+++ src/app/producto-detail/producto-detail.component.ts
@@ -1,16 +1,33 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
 import { Producto } from '../Producto';
+import { ProductoService } from '../producto.service';
+
 @Component({
   selector: 'app-producto-detail',
   templateUrl: './producto-detail.component.html',
   styleUrls: ['./producto-detail.component.css']
 })
 export class ProductoDetailComponent implements OnInit {
-
-@Input() Producto?: Producto;
-  constructor() { }
+  Producto: Producto | undefined;
+  constructor(
+    private route: ActivatedRoute,
+    private productoService: ProductoService,
+    private location: Location
+  ) {}
 
   ngOnInit(): void {
+    this.getProducto();
+  }
+  getProducto(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.productoService.getProducto(id)
+      .subscribe(Producto => this.Producto = Producto);
   }
 
+  goBack(): void {
+    this.location.back();
+  }
 }
